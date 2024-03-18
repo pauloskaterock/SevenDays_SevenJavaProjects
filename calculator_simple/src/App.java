41e754fe1864667c80a5b1e11a4d9d46883455a5
pauloskaterock <pauloengenharia5@yahoo.com>
2024-03-18 16:02:17 -0300

    Programming Java

---
+++ calculator_simple/src/App.java
@@ -0,0 +1,44 @@
+ import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args)  {
+
+       /* Scanner numero1Scanner = new Scanner(System.in);
+        System.out.print("digite o primeiro numero: ");
+
+        int numero1 = numero1Scanner.nextInt();
+        //------------------------------------------------------------
+
+        Scanner numero2Scanner = new Scanner(System.in);
+        System.out.print("digite o segundo numero: ");
+
+        int numero2 = numero2Scanner.nextInt();
+
+        //-----------------------------------------------------
+
+        int resul = numero1 + numero2;
+      
+        System.out.print("o resultado é "  + resul);
+        
+        */
+
+        Scanner numero1Scanner = new Scanner(System.in);
+        System.out.print("digite o primeiro numero: ");
+
+        double numero1 = numero1Scanner.nextDouble();
+        //------------------------------------------------------------
+
+        Scanner numero2Scanner = new Scanner(System.in);
+        System.out.print("digite o segundo numero: ");
+
+        double numero2 = numero2Scanner.nextDouble();
+
+        //-----------------------------------------------------
+
+        double resul = numero1 + numero2;
+      
+        System.out.print("o resultado é "  + resul);
+
+        
+    }
+}
